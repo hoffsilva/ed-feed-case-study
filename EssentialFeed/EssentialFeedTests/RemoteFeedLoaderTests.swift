a7264e4f5e311e7e21298dbb2cb4722f28f5e218
Hoff Silva <hoff.henry@gmail.com>
2020-12-11 15:39:20 -0300

    Move duplicated code to factory method

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -27,30 +27,35 @@ protocol HTTPClient {
     func get(from url: URL)
 }
 
-class HTTPClientSpy: HTTPClient {
-    
-    func get(from url: URL) {
-        requestedURL = url
-    }
-    var requestedURL: URL?
-    
-}
-
 class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(url: nil, client: client)
+        let client = createSUT(with: nil).client
         XCTAssertNil(client.requestedURL)
-        
     }
     
     func test_load_requestDataFromURL() {
-        let url = URL(string: "www.google.com.br")!
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(url: url, client: client)
+        let url = URL(string: "www.google.com")
+        let (sut, client) = createSUT(with: url)
         sut.load()
         XCTAssertEqual(client.requestedURL, url)
+    }
+    
+    // MARK: Helpers
+    
+    private func createSUT(with url: URL? = URL(string: "www.google.com.br")) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
+    }
+    
+    private class HTTPClientSpy: HTTPClient {
+        
+        func get(from url: URL) {
+            requestedURL = url
+        }
+        var requestedURL: URL?
+        
     }
 
 }
