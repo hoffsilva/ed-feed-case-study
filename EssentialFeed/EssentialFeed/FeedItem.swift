fdfdfdc7902acfd560d6280c8be8e99d60c9828a
Hoff Silva <hoff.henry@gmail.com>
2020-12-11 08:50:58 -0300

    Create de feed module and item

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Hoff Henry Pereira da Silva on 11/12/20.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
